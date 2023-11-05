7349a9ae9501bba8ee8707f1b42e02d08135f575
Joowan Park <81845004+parkjoowan97@users.noreply.github.com>
2023-11-06 00:41:48 +0900

    Add files via upload

---
+++ 23.11.09/1주차_BOJ_1051.py
@@ -0,0 +1,15 @@
+N,M = map(int,input().split())
+A = []
+for i in range (0,N):
+    a = list(str(input()))
+    A.append(a)
+ans = []
+for i in range (1,N+1):
+    for x in range (0,N-i+1):
+        for y in range (0,M-i+1):
+            B = list(set([A[x][y],A[x][y+i-1],A[x+i-1][y],A[x+i-1][y+i-1]]))
+            if len(B) == 1:
+                ans.append(i*i)
+            else:
+                continue
+print(max(ans))
